1e4416e20e54e0a1f2cbac3f3df9a951e1e5eafc
JoeSierraHsu <joepm.hsu@gmail.com>
2021-05-13 18:28:36 +0800

    set slick list style

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,8 +63,10 @@ function init() {
             for (var j = 0; j < 5; j++) {
                 if (id == j) {
                     $('.slick_' + id).css('display', 'block');
+                    $('.slick_' + id).css('position', 'relative');
                 } else {
                     $('.slick_' + j).css('display', 'none');
+                    $('.slick_' + j).css('position', 'absolute');
                 }
             }
         });
